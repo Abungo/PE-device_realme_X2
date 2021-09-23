cef3b238192dd8d3d1d04a0d0d5147f70ac6a7c1
Abungo Thokchom <abungothokchom@gmail.com>
2021-09-23 19:40:57 +0530

    AndroidProducts.mk edited.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X2.mk
+    $(LOCAL_DIR)/wave_X2.mk
 
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X2-eng \
-    aosp_X2-user \
-    aosp_X2-userdebug
+    wave_X2-eng \
+    wave_X2-user \
+    wave_X2-userdebug
